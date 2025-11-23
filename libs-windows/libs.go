044a1e879071e1c066ebd03e0cb77a3a59141f31
Oliver Bestmann <oliver.bestmann@googlemail.com>
2025-11-23 14:32:45 +0100

    libs: link to more libraries

--- libs-windows/libs.go
+++ libs-windows/libs.go
@@ -5,6 +5,6 @@ package lib
 #cgo windows,arm64 LDFLAGS: -L${SRCDIR}/arm64 -lwgpu_native
 #cgo windows,386 LDFLAGS: -L${SRCDIR}/386 -lwgpu_native
 
-#cgo windows LDFLAGS: -lopengl32 -lgdi32 -ld3dcompiler_47 -lws2_32 -luserenv -lbcrypt -lntdll -loleaut32
+#cgo windows LDFLAGS: -lopengl32 -lgdi32 -ld3dcompiler_47 -lws2_32 -luserenv -lbcrypt -lntdll -loleaut32 -lole32 -lpropsys
 */
 import "C"
